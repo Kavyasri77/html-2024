bc7cb0aeb304252ffd026c0eeecb287b1337e9cc
Kavyasri77 <154340893+Kavyasri77@users.noreply.github.com>
2024-12-10 22:32:57 +0530

    Add files via upload

---
+++ html-css/CSS/modal/script.js
@@ -0,0 +1,14 @@
+const md=document.getElementById ('modal')
+const btn= document.queryselector ('#btn')
+const closeBtn= document.queryselector('#close')
+function OpenModal()
+{
+    md.style.display='block'
+    document.body.style.
+    backgroundColor="rgba(0,0,0,0.4)"
+}
+function closemodal ()
+{
+    md.style.display="none"
+    document.body.style.backgroundcolor="(rgba25,65,25,0.4)"
+}
